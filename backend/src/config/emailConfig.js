ade07a5b4e51d03d351d9f90ffe584d893d5d681
Collins Muchui <muchuicollins56@gmail.com>
2023-07-06 11:24:34 +0300

    added email confirguration and sendemail in utils

---
+++ backend/src/config/emailConfig.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const email_config= {
+
+    host:'smtp.gmail.com',
+
+    port: 587,
+
+    secure: false,
+
+    requireTLS: true,
+
+    auth: {
+
+        user: process.env.EMAIL_USER,
+
+        pass: process.env.PWD
+
+    }
+
+}
+
+module.exports = email_config
